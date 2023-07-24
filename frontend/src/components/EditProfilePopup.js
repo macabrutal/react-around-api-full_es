22ca359b68a5ec44fd3c09aa9742d62fa2657787
Maca <maca@brutalrocks.com>
2023-07-24 17:38:01 -0400

    Fix:frontend > EditProfilePopup.js and Popup.js

--- frontend/src/components/EditProfilePopup.js
+++ frontend/src/components/EditProfilePopup.js
@@ -1,68 +1,78 @@
 import React from "react";
 import PopupWithForm from "./PopupWithForm";
 
-export default function EditProfilePopup(props){
-    return (<>
-        <PopupWithForm
-            name={"profile"}
-            open={props.open}
-            errors={props.errors}
-            setErrors={props.setErrors}
-            handleClose={props.handleClose}
-        >
-            <div id="profilePopup">
-                <form
-                    id="form"
-                    onSubmit={props.handleSubmitProfile}
-                    action=""
-                    className="popup"
-                    name="edit-profile"
-                    noValidate
-                >
-                    <h4 className="popup__title-popup">Edit profile</h4>
-                    <fieldset className="popup__fieldset">
-                        <div className="popup__field">
-                            <input
-                                id="profileTitle"
-                                className={`popup__input-popup ${
-                                    props.errors.profile.name ? "popup__input-popup_error" : ""
-                                }`}
-                                type="text"
-                                placeholder="Nombre"
-                                name="name"
-                                required
-                                minLength="2"
-                                maxLength="40"
-                            />
-                            <span className="popup__error popup__error_name">
+export default function EditProfilePopup(props) {
+  return (
+    <>
+      <PopupWithForm
+        name={"profile"}
+        open={props.open}
+        errors={props.errors}
+        setErrors={props.setErrors}
+        handleClose={props.handleClose}
+      >
+        <div id="profilePopup">
+          <form
+            id="form"
+            onSubmit={props.handleSubmitProfile}
+            action=""
+            className="popup"
+            name="edit-profile"
+            noValidate
+          >
+            <h4 className="popup__title-popup">Edit profile</h4>
+            <fieldset className="popup__fieldset">
+              <div className="popup__field">
+                <input
+                  id="profileTitle"
+                  className={`popup__input-popup ${
+                    props.errors.profile.name ? "popup__input-popup_error" : ""
+                  }`}
+                  type="text"
+                  placeholder="Nombre"
+                  name="name"
+                  required
+                  minLength="2"
+                  maxLength="40"
+                  value={props.name} // Agregado: muestra el texto a editar en el campo
+                />
+                <span className="popup__error popup__error_name">
                   {props.errors.profile.name}
                 </span>
-                        </div>
+              </div>
 
-                        <div className="popup__field">
-                            <input
-                                id="profileSubtitle"
-                                className="popup__input-popup"
-                                type="text"
-                                placeholder="Acerca de mí"
-                                name="about"
-                                required
-                                minLength="2"
-                                maxLength="200"
-                            />
-                            <span className="popup__error popup__error_about">
+              <div className="popup__field">
+                <input
+                  id="profileSubtitle"
+                  className="popup__input-popup"
+                  type="text"
+                  placeholder="Acerca de mí"
+                  name="about"
+                  required
+                  minLength="2"
+                  maxLength="200"
+                  value={props.about} // Agregado: muestra el texto a editar en el campo
+                />
+                <span className="popup__error popup__error_about">
                   {props.errors.profile.about}
                 </span>
-                        </div>
-                        <button
-                            disabled = {props.isInvalid('profile')}
-                            id="save" type="submit"
-                            className= {`popup__button-popup ${props.isInvalid('profile') ? 'popup__button-popup_inactive' : '' }`}>
-                            Guardar
-                        </button>
-                    </fieldset>
-                </form>
-            </div>
-        </PopupWithForm>
-    </>)
+              </div>
+              <button
+                disabled={props.isInvalid("profile")}
+                id="save"
+                type="submit"
+                className={`popup__button-popup ${
+                  props.isInvalid("profile")
+                    ? "popup__button-popup_inactive"
+                    : ""
+                }`}
+              >
+                Guardar
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </PopupWithForm>
+    </>
+  );
 }
--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 export default function Popup(props) {
+  // Agregamos un manejador de eventos para cerrar el Popup al presionar 'Esc'
+  React.useEffect(() => {
+    function handleEscKey(event) {
+      if (event.key === "Escape") {
+        props.handleClose();
+      }
+    }
+
+    document.addEventListener("keydown", handleEscKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [props.handleClose]);
+
   return (
     <div
       className={`popup-container ${
@@ -16,14 +31,4 @@ export default function Popup(props) {
   );
 }
 
-//LA BASE DE POPUP:
-//1. abre
-//2. cierra
-//3.tiene el contenido de los hijos
 
-// --> CLASE POPUP escondido Y visible
-//popup-container --> escondido
-//popup-container_show --> visible
-
-// --> CLASE BOTÓN X CERRAR
-//popup-container__close-popup
